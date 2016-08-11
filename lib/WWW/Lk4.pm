ab2f4149c069e8148c2ce2fd895cadc58650bc7b
Paul Hoffman <nkuitse@nkuitse.com>
2016-08-11 10:06:02 -0400

    refactor from CGI script to WWW::Lk4

--- lib/WWW/Lk4.pm
+++ lib/WWW/Lk4.pm
@@ -9,6 +9,12 @@ our $VERSION = '0.12';
 
 use constant KEYJOINER => "\x1c";
 
+my %status2msg = (
+    200 => 'OK',
+    302 => 'Found',
+    404 => 'Not found',
+);
+
 # --- Public methods
 
 sub new {
@@ -17,7 +23,9 @@ sub new {
     my %scopes;
     my %file2data;
     my $self = bless {
-        'config_file' => '/etc/lk4/lk4.conf',
+        'config_file' => '/usr/local/lk4/conf/lk4.conf',
+        'config_dir'  => '/usr/local/lk4/conf/lk4.d',
+        'data_dir'    => '/usr/local/lk4/data',
         'data_dir'    => '/var/local/lk4',
         %arg,
         'contexts'    => \@contexts,
@@ -26,6 +34,26 @@ sub new {
     }, $cls;
     $self->load_config;
     return $self;
+}
+
+sub handle_cgi {
+    my ($self, $q) = @_;
+    my %env = $self->request_environment($q);
+    my $req = $env{'$path_info'} . $env{'$query_string'};
+    my %result = $self->resolve($req, %env);
+    my ($ok, $status, $uri, $menu) = @result{qw(ok status uri menu)};
+    if (defined $uri) {
+        $self->print_redirect($uri, $status);
+    }
+    elsif (defined $menu) {
+        $self->print_menu($menu);
+    }
+    elsif (!$ok) {
+        $self->print_error($status);
+    }
+    else {
+        die "Unhandled request: $req";
+    }
 }
 
 sub resolve {
@@ -486,7 +514,138 @@ sub compile_list {
 }
 
 # --- Private methods
-#
+
+sub print_redirect {
+    my ($self, $uri, $status) = @_;
+    $status ||= 302;
+    my $msg = $status2msg{$status} || 'Found';
+    print <<"EOS";
+Status: $status $msg
+Location: $uri
+Content-Type: text/plain
+Content-Length: 0
+
+EOS
+}
+
+sub print_error {
+    my ($self, $status) = @_;
+    $status ||= 404;
+    my $msg = $status2msg{$status} || 'An error occurred';
+    print <<"EOS";
+Status: $status $msg
+
+EOS
+}
+
+sub request_environment {
+    my ($self, $q) = @_;
+    my $uri = $q->url('-absolute' => 1, '-path_info' => 1, '-query' => 1);
+    my @params = map { '$param(' . $_ . ')', $q->param($_) } $q->param;
+    my %cookies = CGI::Cookie->fetch;
+    my @cookies = map { '$cookie(' . $_ . ')', $cookies{$_}->value } keys %cookies;
+    return (
+        '$query_string' => '?'.$q->query_string,
+        '$query_string_unescaped'
+                        => '?'.unescape($q->query_string),
+        '$remote_addr'  => $q->remote_addr,
+        '$path_info'    => $q->path_info,
+        '$url'          => $uri,
+        @params,
+    );
+}
+
+sub unescape {
+    my ($str) = @_;
+    $str =~ s/%([0-9A-Fa-f]{2})/pack('H*', $1)/eg;
+    $str =~ s/;/&/g;
+    return $str;
+}
+
+sub print_menu {
+    my ($self, $menu) = @_;
+    my $template = $menu->{'template'};
+    my $fh;
+    if (defined $template) {
+        my $f = $self->find_menu($template)
+            or die "no such template: $template";
+        open $fh, '<', $f or die "open $template: $!";
+    }
+    else {
+        $fh = \*DATA;
+    }
+    my $content = $self->process_template($fh, {
+        'menu' => $menu,
+    });
+    my $length = length $content;
+    print <<"EOS";
+Status: 200 OK
+Content-Type: text/html; charset=UTF-8
+Content-Length: $length
+
+$content
+EOS
+}
+
+sub process_template {
+    my ($self, $fh, $vars) = @_;
+    my $menu = $vars->{'menu'};
+    my (@header, @item, @footer);
+    my %section = (
+        'header' => \@header,
+        'item' => \@item,
+        'footer' => \@footer,
+    );
+    my $sec = $section{'header'};
+    while (<$fh>) {
+        next if /^#/;
+        if (/^[@]\s*(\w+)/) {
+            $sec = $section{$1} or die "Unrecognized section at line $.: $1";
+        }
+        elsif (/^[|]/) {
+            push @$sec, substr($_, 1);
+        }
+        elsif (/^[-]/) {
+            chomp;
+            push @$sec, substr($_, 1);
+        }
+        elsif (/\S/) {
+            chomp;
+            die "Unrecognized line at line $.: $_";
+        }
+    }
+    my $output = '';
+    foreach (@header) {
+        s/\${\s*(\S+)\s*}/escapeHTML($menu->{$1})/eg;
+        $output .= $_;
+    }
+    foreach my $i (@{ $menu->{'items'} }) {
+        foreach (@item) {
+            my $line = $_;
+            $line =~ s/\${\s*(\S+)\s*}/escapeHTML($i->{$1})/eg;
+            $output .= $line;
+        }
+    }
+    foreach (@footer) {
+        s/\${\s*(\S+)\s*}/escapeHTML($menu->{$1})/eg;
+        $output .= $_;
+    }
+    return $output;
+}
+
+sub find_menu {
+    my ($self, $name) = @_;
+    foreach (grep { defined $_ } $self->{'config_dir'}, basename($self->{'config_file'})) {
+        return "$_/$name.menu" if -f "$_/$name.menu";
+    }
+}
+
+sub basename {
+    local $_ = shift;
+    s{.+/}{};
+    return $_;
+}
+
 sub load_config {
     my ($self) = @_;
     my $root = { 'name' => '', 'stash' => {} };
@@ -710,3 +869,24 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 =cut
+
+__END__
+@header
+|<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+|<html>
+|<head>
+|    <title>${title}</title>
+|</head>
+|<body>
+|<h1>${title}</h1>
+|<p>
+|Please select the appropriate link from the list below.
+|</p>
+|<ul>
+@item
+|<li><a href="${uri}" />${label}</li>
+@footer
+|</ul>
+|</body>
+|</html>
+
