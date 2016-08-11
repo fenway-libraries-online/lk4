b0c6682e7099646dd24e9aaa633a08a41bd07510
Paul Hoffman <nkuitse@nkuitse.com>
2016-08-11 12:47:59 -0400

    fix inline data glitch

--- lib/WWW/Lk4.pm
+++ lib/WWW/Lk4.pm
@@ -871,7 +871,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 =cut
 
-__END__
+__DATA__
 @header
 |<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 |<html>
